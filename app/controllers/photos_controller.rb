fded718beb628022af1a728b2d3e9a4ec25fdcd0
mzamoranoc <mzamoran@chicagobooth.edu>
2025-04-16 06:46:54 +0000

    let's go!

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,27 +1,24 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
 
-    # GET /photos or /photos.json
   def index
     @photos = Photo.all
   end
 
-  # GET /photos/1 or /photos/1.json
   def show
   end
 
-  # GET /photos/new
   def new
     @photo = Photo.new
   end
 
-  # GET /photos/1/edit
   def edit
   end
 
-  # POST /photos or /photos.json
   def create
     @photo = Photo.new(photo_params)
+    # 1) Force the current user to be the owner
+    @photo.owner_id = current_user.id
 
     respond_to do |format|
       if @photo.save
@@ -34,7 +31,6 @@ class PhotosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /photos/1 or /photos/1.json
   def update
     respond_to do |format|
       if @photo.update(photo_params)
@@ -47,25 +43,23 @@ class PhotosController < ApplicationController
     end
   end
 
-  # DELETE /photos/1 or /photos/1.json
   def destroy
     @photo.destroy!
-
     respond_to do |format|
-      format.html { redirect_to photos_path, status: :see_other, notice: "Photo was successfully destroyed." }
+      format.html { redirect_to photos_url, status: :see_other, notice: "Photo was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_photo
-      @photo = Photo.find(params(:id))
+      @photo = Photo.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    # 2) Permit only :image and :caption from the form.
+    #    We don't allow :owner_id, :likes_count, :comments_count from the user input.
     def photo_params
-      params.require(photo: [ :image, :comments_count, :likes_count, :caption, :owner_id ])
+      params.require(:photo).permit(:image, :caption)
     end
-
 end
